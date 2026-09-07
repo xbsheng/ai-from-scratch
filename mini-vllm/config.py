51fef85065e28729450588df2b108c7938e6a4ae
xbsheng <xxbsheng@gmail.com>
2026-09-07 22:56:51 +0800

    feat(mini-vllm): add initial configuration, README, and project metadata

---
+++ mini-vllm/config.py
@@ -0,0 +1,26 @@
+import os
+from dataclasses import dataclass
+
+from transformers import AutoConfig, PreTrainedConfig
+
+
+@dataclass(slots=True)
+class Config:
+    model: str
+    max_num_batched_tokens: int = 16384
+    max_num_seqs: int = 512
+    max_model_len: int = 4096
+    gpu_memory_utilization: float = 0.9
+    tensor_parallel_size: int = 1
+    enforce_eager: bool = False
+    hf_config: PreTrainedConfig | None = None
+    eos: int = -1
+    kv_cache_block_size: int = 256
+    num_kv_cache_blocks: int = -1
+
+    def __post_init__(self):
+        assert os.path.isdir(self.model)
+        assert self.kv_cache_block_size % 256 == 0
+        assert 1 <= self.tensor_parallel_size <= 8
+        self.hf_config = AutoConfig.from_pretrained(self.model)
+        self.max_model_len = min(self.max_model_len, self.hf_config.max_position_embeddings)
